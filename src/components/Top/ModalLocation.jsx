481b878ba2a59ca3fc45b0acc477008660fa2d90
qqvers <bartekszczepanik1999@gmail.com>
2024-02-21 22:43:52 +0100

    Location to fix

--- src/components/Top/ModalLocation.jsx
+++ src/components/Top/ModalLocation.jsx
@@ -3,81 +3,94 @@ import { forwardRef, useState } from "react";
 const ModalLocation = forwardRef(function ModalLocation({ handleSelect }, ref) {
   const [selectedCity, setSelectedCity] = useState("");
 
+  const citiesPoland = ["Kraków", "Śląsk", "Trójmiasto", "Warszawa", "Wrocław"];
+  const citiesWorld = [
+    "Berlin",
+    "London",
+    "New York",
+    "San Francisco",
+    "Sydney",
+  ];
+  const otherCities = [
+    "Białystok",
+    "Bielsko-Biała",
+    "Bydgoszcz",
+    "Częstochowa",
+    "Kielce",
+    "Łódź",
+    "Lublin",
+    "Olsztyn",
+    "Opole",
+    "Poznań",
+    "Rzeszów",
+    "Szczecin",
+    "Toruń",
+    "Zielona Góra",
+  ];
+
   function handleCityName(value) {
     setSelectedCity(value);
   }
 
+  const renderCityButtons = (citiesArray) =>
+    citiesArray.map((city) => (
+      <button
+        key={city}
+        className="location_button_modal"
+        onClick={() => handleCityName(city)}
+      >
+        {city}
+      </button>
+    ));
+
   return (
-    <dialog ref={ref}>
-      <div className="ml_wrapper">
-        <button
-          onClick={() => {
-            handleSelect(null);
-          }}
-        >
-          X
-        </button>
-        <button
-          onClick={() => {
-            handleSelect("Location");
-          }}
-        >
-          Clear filters
-        </button>
-        <h2>Location</h2>
-        <div className="input-container">
-          <input
-            type="text"
-            placeholder="Where do you want to work?"
-            value={selectedCity}
-            onChange={(e) => setSelectedCity(e.target.value)}
-          />
+    <div className="position_modal_location_wrapper">
+      <dialog ref={ref} className="modal_location_dialog">
+        <div className="ml_wrapper">
+          <div className="modal_location_top">
+            <button
+              onClick={() => {
+                handleSelect(null);
+              }}
+            >
+              X
+            </button>
+
+            <h2>Location</h2>
+            <div
+              onClick={() => {
+                handleSelect("Location");
+              }}
+            >
+              Clear filters
+            </div>
+          </div>
+          <div className="input-container">
+            <input
+              type="text"
+              placeholder="Where do you want to work?"
+              value={selectedCity}
+              onChange={(e) => setSelectedCity(e.target.value)}
+            />
+          </div>
+
+          <h3>Top Poland</h3>
+
+          {renderCityButtons(citiesPoland)}
+
+          <h3>Top World</h3>
+          {renderCityButtons(citiesWorld)}
+
+          <h3>Other Locations</h3>
+          {renderCityButtons(otherCities)}
         </div>
-
-        <h3>Top Poland</h3>
-
-        <button onClick={() => handleCityName("Kraków")}>Kraków</button>
-        <button onClick={() => handleCityName("Śląsk")}>Śląsk</button>
-        <button onClick={() => handleCityName("Trójmiasto")}>Trójmiasto</button>
-        <button onClick={() => handleCityName("Warszawa")}>Warszawa</button>
-        <button onClick={() => handleCityName("Wrocław")}>Wrocław</button>
-
-        <h3>Top World</h3>
-
-        <button onClick={() => handleCityName("Berlin")}>Berlin</button>
-        <button onClick={() => handleCityName("London")}>London</button>
-        <button onClick={() => handleCityName("New York")}>New York</button>
-        <button onClick={() => handleCityName("San Francisco")}>
-          San Francisco
-        </button>
-        <button onClick={() => handleCityName("Sydney")}>Sydney</button>
-        <h3>Other Locations</h3>
-
-        <button onClick={() => handleCityName("Białystok")}>Białystok</button>
-        <button onClick={() => handleCityName("Bielsko-Biała")}>
-          Bielsko-Biała
-        </button>
-        <button onClick={() => handleCityName("Bydgoszcz")}>Bydgoszcz</button>
-        <button onClick={() => handleCityName("Częstochowa")}>
-          Częstochowa
-        </button>
-        <button onClick={() => handleCityName("Kielce")}>Kielce</button>
-        <button onClick={() => handleCityName("Łódź")}>Łódź</button>
-        <button onClick={() => handleCityName("Lublin")}>Lublin</button>
-        <button onClick={() => handleCityName("Olsztyn")}>Olsztyn</button>
-        <button onClick={() => handleCityName("Opole")}>Opole</button>
-        <button onClick={() => handleCityName("Poznań")}>Poznań</button>
-        <button onClick={() => handleCityName("Rzeszów")}>Rzeszów</button>
-        <button onClick={() => handleCityName("Szczecin")}>Szczecin</button>
-        <button onClick={() => handleCityName("Toruń")}>Toruń</button>
-        <button onClick={() => handleCityName("Zielona Góra")}>
-          Zielona Góra
-        </button>
-      </div>
-      <div className="button-show-offers">
-        <button onClick={() => handleSelect(selectedCity)}>Show offers</button>
-      </div>
-    </dialog>
+        <div className="button-show-offers">
+          <button onClick={() => handleSelect(selectedCity)}>
+            Show offers
+          </button>
+        </div>
+      </dialog>
+    </div>
   );
 });
 
